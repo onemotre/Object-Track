e8ded9db939655013259c0f831dcba9e47c44926
onemotre <130.toolwolrd@gmail.com>
2023-09-02 13:54:25 +0800

    update examples
    上传样例视频

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/onemotre/Documents/T-DT/study-task/Object-Track/Object-Track
+# Build directory: /home/onemotre/Documents/T-DT/study-task/Object-Track/Object-Track/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
